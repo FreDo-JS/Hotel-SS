de9255771d4c9142f1f6e9ce8b581584d6ee651c
myNameIsJeff <123896394+FreDo-JS@users.noreply.github.com>
2024-11-09 21:23:41 +0100

    Starting Files

---
+++ Hotel SS-IGMA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotel_SS_IGMA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
